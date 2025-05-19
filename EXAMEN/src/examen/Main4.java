0127af8b97699382b7adb5e7aae7df8ddf65a222
RodrigoMaddonni <rfm00@iesemilidarder.com>
2025-05-19 17:12:45 +0200

    FinalCorregido

--- EXAMEN/src/examen/Main4.java
+++ EXAMEN/src/examen/Main4.java
@@ -12,33 +12,30 @@ public class Main4 {
     public static void main(String[] args) {
         List<Vehicle> vehicles = new ArrayList<>();
         final String arxiu = "c:\\temp\\mysql.con";
-        String sql = "SELECT matricula, marca, model, any, preu FROM vehicles";
+        int anyFiltrat = 2020;
         GestorBBDD gestorBBDD = new GestorBBDD(arxiu);
         
         try (Connection conn = gestorBBDD.getConnectionFromFile();
-              ResultSet resultSet = gestorBBDD.executaQuerySQL(conn, sql) ) {   
-            
-            while (resultSet.next())
-                afageixVehicle(vehicles, new Vehicle(resultSet.getString("matricula"),
+                ResultSet resultSet = gestorBBDD.executaQuerySQL(conn, "SELECT matricula, marca, model, any, preu FROM vehicles where any >= ?", anyFiltrat) ) {  
+                while (resultSet.next())
+                    afageixVehicle(vehicles, new Vehicle(resultSet.getString("matricula"),
                                                     resultSet.getString("marca"), 
                                                     resultSet.getString("model"),
                                                     resultSet.getInt("any"),
-                                                    resultSet.getDouble("preu"))
-                             );
-            
+                                                    resultSet.getDouble("preu")));
         } catch (SQLException e) {
             System.err.println("Error carregant vehicles BBDD: " + e.getMessage());
         } catch (IOException e) {
             System.err.println("Error conectant BBDD: " + e.getMessage());
         }
         
-        
+        // Imprimir
         vehicles.stream().forEach(v -> System.out.println(v));
     }
     
     
     
-    
+    // Metodo para agregar vehículo
     private static void afageixVehicle(List<Vehicle> vehicles, Vehicle v) {
         vehicles.add(v);
     }
